2d189327a430833803d9b26af537faa1aa6f13e4
xpozer <sdone@live.de>
2026-05-01 16:08:56 +0200

    feat(screen): export privacy config modules

--- backend/perception/screen/__init__.py
+++ backend/perception/screen/__init__.py
@@ -1,9 +1,13 @@
 # backend/perception/screen/__init__.py
 from .capturer import ScreenCapturer, ScreenCaptureConfig
+from .config import ScreenPerceptionConfig, ScreenPrivacyConfig, load_screen_config, save_screen_config
 from .context_builder import ScreenContextBuilder
+from .hotkeys import ScreenHotkeyConfig, ScreenHotkeyRegistry
+from .indicator import ScreenActivityIndicator
 from .models import ActiveWindowInfo, PrivacyDecision, ScreenContext, ScreenshotFrame, VisionResult
 from .ocr_fallback import OcrFallback
 from .privacy import ScreenPrivacyPolicy
+from .runtime_state import ScreenRuntimeState
 from .vision import ScreenVisionAnalyzer
 from .window_tracker import WindowTracker
 
@@ -11,13 +15,21 @@ __all__ = [
     "ActiveWindowInfo",
     "OcrFallback",
     "PrivacyDecision",
+    "ScreenActivityIndicator",
     "ScreenCaptureConfig",
     "ScreenCapturer",
     "ScreenContext",
     "ScreenContextBuilder",
+    "ScreenHotkeyConfig",
+    "ScreenHotkeyRegistry",
+    "ScreenPerceptionConfig",
+    "ScreenPrivacyConfig",
     "ScreenPrivacyPolicy",
+    "ScreenRuntimeState",
     "ScreenVisionAnalyzer",
     "ScreenshotFrame",
     "VisionResult",
     "WindowTracker",
+    "load_screen_config",
+    "save_screen_config",
 ]
