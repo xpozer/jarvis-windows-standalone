d1328b53738cd1d2bf3404e84612800aedb9c4a6
xpozer <sdone@live.de>
2026-05-02 08:55:43 +0200

    feat: add storybook main config

---
+++ frontend/.storybook/main.ts
@@ -0,0 +1,16 @@
+// frontend/.storybook/main.ts
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.stories.@(ts|tsx)"],
+  addons: ["@storybook/addon-essentials", "@storybook/addon-links", "@storybook/addon-interactions"],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+
+export default config;
